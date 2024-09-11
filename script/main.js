05428cd1757e44272b68f08528aef96aea5d09dd
Guesmia Osmane <osmane688@hotmail.com>
2024-09-11 14:53:04 +0200

    kd

--- script/main.js
+++ script/main.js
@@ -1,7 +1,17 @@
-function createTable(DOMelement, datas){
+const createTable = function createTable(DOMelement, datas){
     let element = DOMelement;
-    let table = document.createElement('table');
+    const table = document.createElement('table')
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    table.appendChild(thead);
+    table.appendChild(tbody);
     
+    const thead_tr = document.createElement('tr');
+    thead.appendChild(thead_tr);
 
-    element.appendChild(table);
+    for(prop in datas.skills[0]){
+        const th = document.createElement('th');
+        th.innerText = prop;
+        thead_tr.appendChild(tr);
+    }
 }
